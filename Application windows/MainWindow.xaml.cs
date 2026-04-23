6f0135b3cf64adaa1857299b9ba2c8d5ccc2128e
KitaKit <sositeherstupidmans@gmail.com>
2026-04-23 11:47:49 +0200

    A minor optimization.

--- Application windows/MainWindow.xaml.cs
+++ Application windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using GenotypeApplication.MVVM.TreeView;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +29,22 @@ namespace GenotypeApplication
         {
             App.GlobalCts.Cancel();
             base.OnClosing(e);
+        }
+
+        private void OpenInExplorer_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem menuItem &&
+                menuItem.DataContext is FileNodeViewModel node)
+            {
+                var path = node.IsDirectory
+                    ? node.FullPath
+                    : System.IO.Path.GetDirectoryName(node.FullPath);
+
+                if (Directory.Exists(path))
+                {
+                    Process.Start("explorer.exe", path);
+                }
+            }
         }
     }
 }
